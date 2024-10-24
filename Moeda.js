c184a3e8c94058b11d6cfd1643e874673a36e4df
luamy.gama@alunoesocolassociais.org.br <luamy.gama@alunoesocolassociais.org.br>
2024-10-24 09:56:20 -0300

    Fazendo o codigo que Definr taxa de cambio fixa com utilização de objeto

--- Moeda.js
+++ Moeda.js
@@ -1,7 +1,10 @@
 document.getElementById('currency-form').addEventListener('submit', function(event){
-    
+
 // Obter valores de entrada do formulario
+const valor = parseFloat(docment.getElementById('amount').value);
+const moeda1 = document.getElementById('moeda1').value;
+const moeda2 = document.getElementById('moeda2').value;
 
-
-
+// Definir taxa de cambio fixa com utilização de objeto
+const exchangeRates = {}; 
 });
