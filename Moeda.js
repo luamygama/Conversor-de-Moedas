cfb71686974348a1dde241998f7c292671a7cf41
luamy.gama@alunoesocolassociais.org.br <luamy.gama@alunoesocolassociais.org.br>
2024-10-24 10:09:18 -0300

    adiconando valores de cada taxa

--- Moeda.js
+++ Moeda.js
@@ -6,5 +6,9 @@ const moeda1 = document.getElementById('moeda1').value;
 const moeda2 = document.getElementById('moeda2').value;
 
 // Definir taxa de cambio fixa com utilização de objeto
-const exchangeRates = {}; 
+const exchangeRates = {
+    USD: {EUR: 0.93, BRL: 5.71 },
+    BRL: {EUR: 0.16, USD: 0.18 },
+    EUR: {BRL: 6.16, USD: 1.08 }
+}; 
 });
